b97930938b6f7154d3a4eed33ee38fca02d7c7f4
Camila <camilakadi@hotmail.com>
2020-09-06 17:43:39 -0300

    ajustes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,12 +14,6 @@ export interface Repos {
 };
 
 function Home() {
-    const searchIconStyle = {
-        stroke: "none",
-        fill: "#ccc",
-        backgroundColor: "transparent"
-    };
-
     const [repos, setRepos] = useState([]);
     const [search, setSearch] = useState('');
 
@@ -38,72 +32,70 @@ function Home() {
             }
         }
         else {
-            //alert('Necessário digitar um nome de usuário');
             setRepos([]);
         }
-
-
     }
 
     return (
-        <div id="wrapper">
+        <div className="wrapper">
             <header>
                 <div className="container top-container">
                     <div className="logo">GitHub Viewer</div>
                     <form className="searchs__form" onSubmit={pesquisarGitHub}>
-                        <fieldset>
-                            <input type="text" id="search" name="search" className="searchs__input" value={search}
-                                onChange={(e) => {setSearch(e.target.value)}}
-                            />
-                        </fieldset>
+                        <input type="text" id="search" name="search" className="searchs__input" value={search}
+                            onChange={(e) => {setSearch(e.target.value)}} placeholder="Buscar Repositório"
+                        />
                         <button className="searchs__button">
                             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-1 0 136 136.21852">
                                 <g id="surface1">
                                     <path d="M 93.148438 80.832031 C 109.5 57.742188 104.03125 25.769531 80.941406 9.421875 C 57.851562 -6.925781 25.878906 -1.460938 9.53125 21.632812 C -6.816406 44.722656 -1.351562 76.691406 21.742188 93.039062 C 38.222656 104.707031 60.011719 105.605469 77.394531 95.339844 L 115.164062 132.882812 C 119.242188 137.175781 126.027344 137.347656 130.320312 133.269531 C 134.613281 129.195312 134.785156 122.410156 130.710938 118.117188 C 130.582031 117.980469 130.457031 117.855469 130.320312 117.726562 Z M 51.308594 84.332031 C 33.0625 84.335938 18.269531 69.554688 18.257812 51.308594 C 18.253906 33.0625 33.035156 18.269531 51.285156 18.261719 C 69.507812 18.253906 84.292969 33.011719 84.328125 51.234375 C 84.359375 69.484375 69.585938 84.300781 51.332031 84.332031 C 51.324219 84.332031 51.320312 84.332031 51.308594 84.332031 Z M 51.308594 84.332031 "
-                                        style={searchIconStyle}
+
                                     />
                                 </g>
                             </svg>
                         </button>
                     </form>
                 </div>
-
             </header>
 
             <main>
                 <div className="content">
                     <div className="container">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>nome</th>
-                                    <th>criado em</th>
-                                    <th>descrição</th>
-                                    <th>linguagem</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    repos.map((repo: Repos) => {
-                                        return(
-                                            <tr key={repo.id}>
-                                                <td>{repo.id}</td>
-                                                <td>{repo.name}</td>
-                                                <td>{format(new Date(repo.created_at), 'dd/MM/yyyy')}</td>
-                                                <th>{repo.description}</th>
-                                                <td>{repo.language}</td>
-                                            </tr>
-                                        );
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                        <div className="scrollable-table">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Nome</th>
+                                        <th>Data da Criação</th>
+                                        <th>Descrição</th>
+                                        <th>Linguagem</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        repos.map((repo: Repos) => {
+                                            return(
+                                                <tr key={repo.id}>
+                                                    <td>{repo.id}</td>
+                                                    <td>{repo.name}</td>
+                                                    <td>{format(new Date(repo.created_at), 'dd/MM/yyyy')}</td>
+                                                    <td>{repo.description}</td>
+                                                    <td>{repo.language}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </main>
             <footer>
-
+                <div className="container">
+                    &copy; Camila Kadi Garcia - Todos os direitos reservados - 2020
+                </div>
             </footer>
         </div>
     )
